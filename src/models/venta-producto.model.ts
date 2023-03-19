35a24d85d0a28c407e7405fa771d763a3c06843a
unknown <cristian.gonzalez22582@ucaldas.edu.co>
2023-03-19 15:14:41 -0500

    Creacion de modelos, repositorios y sql de la logica de negocio

---
+++ src/models/venta-producto.model.ts
@@ -0,0 +1,51 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model({settings: {strict: false}})
+export class VentaProducto extends Entity {
+  @property({
+    type: 'number',
+    id: true,
+    generated: true,
+  })
+  id?: number;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  idVenta: number;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  idProducto: number;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  cantidad: number;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  precioUnitario: number;
+
+  // Define well-known properties here
+
+  // Indexer property to allow additional data
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  [prop: string]: any;
+
+  constructor(data?: Partial<VentaProducto>) {
+    super(data);
+  }
+}
+
+export interface VentaProductoRelations {
+  // describe navigational properties here
+}
+
+export type VentaProductoWithRelations = VentaProducto & VentaProductoRelations;
---
+++ src/models/venta.model.ts
@@ -0,0 +1,51 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model({settings: {strict: false}})
+export class Venta extends Entity {
+  @property({
+    type: 'number',
+    id: true,
+    generated: true,
+  })
+  id?: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  numero: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  fecha: string;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  idCliente: number;
+
+  @property({
+    type: 'boolean',
+    required: true,
+  })
+  notificada: boolean;
+
+  // Define well-known properties here
+
+  // Indexer property to allow additional data
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  [prop: string]: any;
+
+  constructor(data?: Partial<Venta>) {
+    super(data);
+  }
+}
+
+export interface VentaRelations {
+  // describe navigational properties here
+}
+
+export type VentaWithRelations = Venta & VentaRelations;
